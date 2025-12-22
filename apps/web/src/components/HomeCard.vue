8ed5293b1d071132a2a2f2fab36991c743f030fe
Sean Boaden <sean.b0153@gmail.com>
2025-12-22 13:37:24 +0800

    fix: responsiveness

--- apps/web/src/components/HomeCard.vue
+++ apps/web/src/components/HomeCard.vue
@@ -1,12 +1,21 @@
-<!-- 
-  File contains the component that displays the featured movie 
-  to the user at the top of the Homepage. Basic display has been made, 
+<!--
+  File contains the component that displays the featured movie
+  to the user at the top of the Homepage. Basic display has been made,
   though currents lacks functionality (i.e. buttons)
 -->
 
 <script setup lang="ts">
-import { Card, CardContent, CardDescription, CardFooter, CardHeader, CardTitle,} from '@/components/ui/card'
+import { Button } from '@/components/ui/button'
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardFooter,
+  CardHeader,
+  CardTitle,
+} from '@/components/ui/card'
 import { getImage } from '@/lib/utils'
+import { useColorMode } from '@vueuse/core'
 import moment from 'moment'
 
 const { name, releaseDate, description, banner } = defineProps<{
@@ -16,106 +25,85 @@ const { name, releaseDate, description, banner } = defineProps<{
   description?: string
   banner?: string
 }>()
+const mode = useColorMode()
 </script>
 
 <template>
-
-  <Card class="flex h-120 max-w-screen">
-    <div v-if="banner" class="banner" 
-      :style="{backgroundImage: `url('${getImage(banner)}')`, 
-    }">
-      <div class="overlay">
-        <div class="movieInfo">
-          <CardHeader >
-            <CardTitle class="text-4xl"> {{ name }}</CardTitle>
-            <CardDescription>{{ moment(releaseDate).get('year') }}</CardDescription>
-          </CardHeader>
-          <CardContent>
-            <p class="description">{{ description }}</p>
-          </CardContent>
-          <CardFooter class="gap-5">
-            <!-- TODO: Add functionality to buttons.-->
-            <button class="button"> Watch now</button> <!-- NOTE: May run into issues where if movie desc is too long, buttons will go off screen. Yet to test if this is true. -->
-            <button class="button"> More Info</button>
-          </CardFooter>
-        </div>
-      </div>
+  <Card
+    class="home-card flex flex-col flex-1 w-full max-w-full relative rounded-lg overflow-hidden"
+  >
+    <div
+      v-if="banner"
+      class="banner w-full h-full z-0"
+      :style="{ backgroundImage: `url('${getImage(banner)}')` }"
+    >
+      <div v-if="mode === 'dark'" class="overlay"></div>
     </div>
-
+    <CardHeader class="z-10">
+      <CardTitle class="text-4xl font-bold text-white">
+        {{ name }}
+      </CardTitle>
+      <CardDescription class="max-w-1/3 text-white">
+        {{ moment(releaseDate).get('year') }}
+      </CardDescription>
+    </CardHeader>
+    <CardContent class="z-10">
+      <p
+        class="description text-white text-shadow-md text-shadow-black opacity-85"
+        style="max-width: clamp(200px, 50%, 600px)"
+      >
+        {{ description }}
+      </p>
+    </CardContent>
+    <CardFooter class="mt-5 gap-5 z-10">
+      <!-- TODO: Add functionality to buttons.-->
+      <Button class="button">Watch now</Button>
+      <!-- NOTE: May run into issues where if movie desc is too long, buttons will go off screen. Yet to test if this is true. -->
+      <Button variant="secondary" class="button">More Info</Button>
+    </CardFooter>
   </Card>
 </template>
 
 <!-- CSS stuff, Very messy atm but will redo it better -->
-<style scoped>
-  .title {
-    display: flex;
-    font-size: 48px;
-    font-weight: bold;
-    padding-left: 0px;
-    background-repeat: no-repeat;
-  }
+<style scoped lang="scss">
+.home-card {
+  height: 70vh;
+}
+.title {
+  font-size: 3rem;
+  font-weight: bold;
+}
 
-  /* Adjusts width and length of the banner */
-  .banner {
-    background-size: cover;
-    height: 130%;
-    width: 170%;
-    background-position: center;
-  }
+/* Adjusts width and length of the banner */
+.banner {
+  position: absolute;
+  background-repeat: no-repeat;
+  background-size: cover;
+  width: 100%;
 
   /* Adjusts the grey scale overlay  */
   .overlay {
-    position: relative;
+    position: absolute;
     height: 100%;
     width: 100%;
-    background:
-      linear-gradient(
-        to bottom,
-        var(--color-background) -8%,
-         rgba(36, 35, 35, 0.2) 20%,
-         rgba(14, 13, 13, 0.4) 60%,
-          var(--color-background) 100%
-      )
+    background: linear-gradient(
+      to bottom,
+      var(--color-background) -8%,
+      rgba(36, 35, 35, 0.2) 20%,
+      rgba(14, 13, 13, 0.4) 60%,
+      var(--color-background) 100%
+    );
   }
-
-  /* Adjusts variables for movie information  */
-  .movieInfo {
-    position: relative;
-    top: 0;
-    height: 100vh;
-    max-width: 600px;
-    padding: 2rem;
-
-    background:
-      linear-gradient(
-        to right,
-          var(--color-background) 0%,
-         rgba(107, 107, 107, 0.2) 85%,
-         rgba(46, 45, 45, 0) 98%
-
-      )
-  }
-
-  .description{
-    font-size: 20px;
-    font-weight: bold;
-  }
-
-.button {
-  padding: 15px 30px;
-  color: #ffffff;
-  background-color: var(--color-primary);
-  border-radius: 5px;
-  font-size: 18px;
-  font-weight: bold;
-  cursor: pointer;
-  transition: background-color 0.3s ease, box-shadow 0.3s ease;
-  margin-top: 40px;
-  box-shadow: 0 4px 6px rgba(0, 0, 0, 0.3); 
 }
 
-.button:hover {
-  background-color: var(--color-chart-5);
+.description {
+  font-size: 0.9rem;
+}
+
+.button {
+  cursor: pointer;
+  box-shadow: 0 4px 6px rgba(0, 0, 0, 0.3);
+  transition: transform 0.1s ease-in-out;
 }
 
 .button:active {
--- apps/web/src/views/HomeView.vue
+++ apps/web/src/views/HomeView.vue
@@ -1,4 +1,4 @@
-<!-- 
+<!--
   File contains the structure and components for the Homepage.
   The homepage is made up of the following components:
     -> HomeCard.vue
@@ -25,62 +25,41 @@ onMounted(async () => {
   // await mediaStore.startScanProgress()
 })
 
-// Example featured movie ID is 'Inside Out', choosing featured movie will eventually be automated.
-const featuredMovieID = "3a914481-22a4-4124-9d98-654cb2f30c45"
-const featuredMovie = computed(() => 
-  movies.value.find(m => m.id === featuredMovieID) 
+// Featured movie is a random media for now
+const featuredMovie = computed(() =>
+  movies.value.at(Math.floor(Math.random() * movies.value.length)),
 )
 
-// testing carousel with movies, will eventually create backend functionality 
-const recommendedMovies = computed(() =>
-  movies.value.slice(2, 12)
-)
+// testing carousel with movies, will eventually create backend functionality
+const recommendedMovies = computed(() => movies.value.slice(2, 12))
 
-const topMovies = computed(() =>
-  movies.value.slice(0, 10)
-)
+const topMovies = computed(() => movies.value.slice(0, 10))
 
-const topSeries = computed(() =>
-  series.value.slice(0, 10)
-)
-
+const topSeries = computed(() => series.value.slice(0, 10))
 </script>
 <template>
-  <div>
-    <!-- HomeCard displays a featured movie on the homescreen, right now it is labled as 'Continue Watching and only displays one movie atm.'-->  
-    <HomeCard       
-      v-if="featuredMovie"
-      :key="featuredMovie.id"
-      :id="featuredMovie.id"
-      :name="featuredMovie.info?.name"
-      :thumbnail="featuredMovie.info?.thumbnail"
-      :banner="featuredMovie.info?.banner"
-      :description="featuredMovie.info?.description"
-      :releaseDate="featuredMovie.info?.releaseDate"
-    />
-  </div>
-  <div>
-    <!-- Navbar option like the one in dribbble example, HomePage currently has netflix style arrangement. --> 
-    <!-- <HomeNavbar /> --> 
-  </div>
-  <div class="carousel"> 
-    <HomeCarousel 
-      :listType="'Recommended'"
-      :mediaList="recommendedMovies" 
+  <div class="w-full h-full max-h-[calc(100vh-96px)] rounded-lg overflow-y-auto flex flex-col">
+    <div class="w-full h-auto">
+      <!-- HomeCard displays a featured movie on the homescreen, right now it is labled as 'Continue Watching and only displays one movie atm.'-->
+      <HomeCard
+        v-if="featuredMovie"
+        :key="featuredMovie.id"
+        :id="featuredMovie.id"
+        :name="featuredMovie.info?.name"
+        :thumbnail="featuredMovie.info?.thumbnail"
+        :banner="featuredMovie.info?.banner"
+        :description="featuredMovie.info?.description"
+        :releaseDate="featuredMovie.info?.releaseDate"
       />
-    <HomeCarousel 
-      :listType="'Top 10 Movies'"
-      :mediaList="topMovies"
-    />
-    <HomeCarousel 
-      :listType="'Top 10 Series'"
-      :mediaList="topSeries"
-    />
+    </div>
+    <div>
+      <!-- Navbar option like the one in dribbble example, HomePage currently has netflix style arrangement. -->
+      <!-- <HomeNavbar /> -->
+    </div>
+    <div class="w-full">
+      <HomeCarousel :listType="'Recommended'" :mediaList="recommendedMovies" />
+      <HomeCarousel :listType="'Top 10 Movies'" :mediaList="topMovies" />
+      <HomeCarousel :listType="'Top 10 Series'" :mediaList="topSeries" />
+    </div>
   </div>
 </template>
-
-<style scoped>
-  .carousel {
-    margin-top: 1px;
-  }
-</style>
