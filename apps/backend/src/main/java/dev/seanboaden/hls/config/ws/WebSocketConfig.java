64b920ff04d64a6e29812bd3d3720f72afcfcc61
Sean Boaden <sean.b0153@gmail.com>
2025-12-23 20:14:42 +0800

    add WebSocketHandshakeInterceptor to attach User to session

--- apps/backend/src/main/java/dev/seanboaden/hls/config/ws/WebSocketConfig.java
+++ apps/backend/src/main/java/dev/seanboaden/hls/config/ws/WebSocketConfig.java
@@ -7,6 +7,7 @@ import org.springframework.web.socket.config.annotation.EnableWebSocket;
 import org.springframework.web.socket.config.annotation.WebSocketConfigurer;
 import org.springframework.web.socket.config.annotation.WebSocketHandlerRegistry;
 
+import dev.seanboaden.hls.config.interceptor.WebSocketHandshakeInterceptor;
 import dev.seanboaden.hls.messaging.handler.IncomingWsMessageHandler;
 
 @Configuration
@@ -14,10 +15,13 @@ import dev.seanboaden.hls.messaging.handler.IncomingWsMessageHandler;
 public class WebSocketConfig implements WebSocketConfigurer {
   @Autowired
   private IncomingWsMessageHandler handler;
+  @Autowired
+  private WebSocketHandshakeInterceptor webSocketHandshakeInterceptor;
 
   @Override
   public void registerWebSocketHandlers(@NonNull WebSocketHandlerRegistry registry) {
     registry.addHandler(handler, "/ws")
+        .addInterceptors(webSocketHandshakeInterceptor)
         .setAllowedOrigins("*");
   }
 }
--- apps/backend/src/main/java/dev/seanboaden/hls/session/service/SessionWrapper.java
+++ apps/backend/src/main/java/dev/seanboaden/hls/session/service/SessionWrapper.java
@@ -7,6 +7,7 @@ import dev.seanboaden.hls.logging.Logging;
 import dev.seanboaden.hls.messaging.dto.ErrorResponse;
 import dev.seanboaden.hls.messaging.dto.InfoResponse;
 import dev.seanboaden.hls.messaging.model.AbstractWebSocketMessage;
+import dev.seanboaden.hls.user.model.User;
 import lombok.Data;
 import org.springframework.web.socket.TextMessage;
 import org.springframework.web.socket.WebSocketSession;
@@ -47,10 +48,17 @@ public class SessionWrapper {
   }
 
   public String getUserId() {
-    Object userId = this.session.getAttributes().get("userId");
-    if (userId == null)
+    User user = this.getUser();
+    if (user == null)
       return null;
-    return userId.toString();
+    return user.getId();
+  }
+
+  public User getUser() {
+    Object userAttribute = this.session.getAttributes().get("user");
+    if (userAttribute == null)
+      return null;
+    return (User) userAttribute;
   }
 
   private void sendMessageSafe(String message) {
